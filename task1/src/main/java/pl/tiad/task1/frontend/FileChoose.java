927fa229ba6801cb80d650b40511841aa4ff4f95
Radosław Bucki <236507@edu.p.lodz.pl>
2023-03-18 17:34:18 +0100

    Fix issues

--- task1/src/main/java/pl/tiad/task1/frontend/FileChoose.java
+++ task1/src/main/java/pl/tiad/task1/frontend/FileChoose.java
@@ -3,11 +3,10 @@ package pl.tiad.task1.frontend;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
-import javafx.stage.Window;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
 
 public class FileChoose {
     private FileChoose() {
@@ -16,68 +15,29 @@ public class FileChoose {
     private static String lastUsedDir = "";
 
     /**
-     * Method opens dialog to choose location to save.
-     * @param windowTitle String
-     * @param actionEvent ActionEvent
-     * @return String
-     * @throws NoSuchMethodException exception
-     * @throws InvocationTargetException exception
-     * @throws IllegalAccessException exception
-     */
-    public static String saveChooser(String windowTitle, ActionEvent actionEvent)
-            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        String choose = null;
-        choose = choose(
-                windowTitle, actionEvent,
-                FileChooser.class.getMethod("showSaveDialog", Window.class)
-        );
-        return choose;
-    }
-
-    /**
-     * Method opens dialog to choose file to open.
-     * @param windowTitle String
-     * @param actionEvent ActionEvent
-     * @return String
-     * @throws NoSuchMethodException exception
-     * @throws InvocationTargetException exception
-     * @throws IllegalAccessException exception
-     */
-    public static String openChooser(String windowTitle, ActionEvent actionEvent)
-            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        String choose = null;
-        choose = choose(
-                windowTitle, actionEvent,
-                FileChooser.class.getMethod("showOpenDialog", Window.class)
-        );
-        return choose;
-    }
-
-    /**
      * Method opens given type of dialog.
+     *
      * @param windowTitle String
      * @param actionEvent ActionEvent
      * @return String
-     * @throws InvocationTargetException exception
-     * @throws IllegalAccessException exception
      */
-    private static String choose(String windowTitle, ActionEvent actionEvent, Method showDialog)
-            throws InvocationTargetException, IllegalAccessException {
+    public static List<String> choose(
+            String windowTitle,
+            ActionEvent actionEvent
+    ) {
         FileChooser chooser = new FileChooser();
         chooser.setTitle(windowTitle);
         if (!lastUsedDir.isEmpty()) {
             chooser.setInitialDirectory(new File(lastUsedDir));
         }
-        File chosenFile = (File) showDialog.invoke(
-                chooser,
-                ((Button) actionEvent.getSource())
-                        .getScene()
-                        .getWindow()
-        );
-        if (chosenFile == null) {
-            return "";
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Articles data (*.csv)", "*.csv"));
+        List<File> chosenFiles = chooser.showOpenMultipleDialog(((Button) actionEvent.getSource())
+                .getScene()
+                .getWindow());
+        if (chosenFiles == null) {
+            return Collections.emptyList();
         }
-        lastUsedDir = chosenFile.getAbsoluteFile().getParentFile().getAbsolutePath();
-        return chosenFile.getAbsolutePath();
+        lastUsedDir = chosenFiles.get(0).getAbsoluteFile().getParentFile().getAbsolutePath();
+        return chosenFiles.stream().map(File::getAbsolutePath).toList();
     }
 }
